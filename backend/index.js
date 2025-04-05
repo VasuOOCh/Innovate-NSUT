d16abce7a89842b5b05934681fd4ca68ac1002be
VasuOOCh <vasu2005choudhari@gmail.com>
2025-04-05 12:36:39 +0530

    added auth

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,10 @@ import cors from "cors";
 import path from "path";
 import {connectDB} from "./lib/connectDb.js";
 
+// Import the routers :
+import authRouter from "./routers/authRoute.js"
 
-// import authRoutes from "./routes/userRoutes.js";
-// import messageRoutes from "./routes/messageRoute.js";
+
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
@@ -17,6 +18,7 @@ const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 app.use(express.json());
+app.use(express.urlencoded({extended : true}))
 app.use(cookieParser());
 app.use(
   cors({
@@ -36,11 +38,20 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+app.use('/api/auth', authRouter);
 
-app.use("/", (req, res) => {
-  res.send("API is running");
-}
-);
+app.use((err,req,res,next) => {
+  console.log( "Error is : ", err);
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong..."
+
+  res.status(status).json({
+    status,
+    message,
+    error : true
+  })
+})
+
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
   connectDB();
